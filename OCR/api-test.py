c910e4d173b41936d098463faed86ea28fc2a85f
Hutaba-Makura <152959976+Hutaba-Makura@users.noreply.github.com>
2024-12-03 15:32:59 +0900

    fix: 正しい座標に描画出来るようにした

--- OCR/api-test.py
+++ OCR/api-test.py
@@ -42,25 +42,46 @@ def detect_text(image_path):
 # 結果をもとに各領域を赤枠で囲う
 def draw_text_area(image_path, response):
     image = cv2.imread(image_path)
+    original_height, original_width = image.shape[:2]
+
     for text in response.text_annotations:
         vertices = text.bounding_poly.vertices
-        # すべての座標を時計回りに90度回転させる
-        points = [(vertex.y, vertex.x) for vertex in vertices if vertex.x is not None and vertex.y is not None]
+        # 座標変換：Cloud Vision (右上原点) -> OpenCV (左上原点)
+        points = [
+            (int(original_width-vertex.y), int(vertex.x))  # Y 座標を反転
+            for vertex in vertices if vertex.x is not None and vertex.y is not None
+        ]
+
+        # 頂点の順序を修正して回転を正す←いらなくね？
         if len(points) == 4:
-            cv2.polylines(image, [np.array(points, dtype=np.int32)], isClosed=True, color=(0, 0, 255), thickness=2)
+            cv2.polylines(image, [np.array(points, dtype=np.int32)], isClosed=True, color=(0, 0, 255), thickness=2) # 赤枠
         else:
             print("頂点数が不足しているためスキップしました:", points)
     
+    # 原点に黒丸を描画　50pxくらいの大きさ
+    cv2.circle(image, (0, 0), 50, (0, 0, 0), thickness=-1)
+
     # 一番大きい領域を青枠で囲う
     if len(response.text_annotations) > 0:
         vertices = response.text_annotations[0].bounding_poly.vertices
-        points = [(vertex.y, vertex.x) for vertex in vertices if vertex.x is not None and vertex.y is not None]
+        points = [
+            (int(original_width-vertex.y), int(vertex.x))  # Y 座標を反転
+            for vertex in vertices if vertex.x is not None and vertex.y is not None
+        ]
+
+        # points[0]に青丸を描画
+        cv2.circle(image, (points[0][1], points[0][0]), 25, (255, 0, 0), thickness=-1)
+
         if len(points) == 4:
-            cv2.polylines(image, [np.array(points, dtype=np.int32)], isClosed=True, color=(255, 0, 0), thickness=2)
+            cv2.polylines(image, [np.array(points, dtype=np.int32)], isClosed=True, color=(255, 0, 0), thickness=2)  # 青枠
         else:
             print("頂点数が不足しているためスキップしました:", points)
+
     cv2.imwrite('text_area.jpg', image)
     print("テキスト領域をtext_area.jpgに保存しました。")
+
+
+
 
 # テキスト検出
 image_path = r".\samples\DSC_1937.JPG"
