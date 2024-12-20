9e07a0f14b8b5fd211c04469337f39d64e6df60f
Hutaba-Makura <src1258372452486242304@gmail.com>
2024-12-20 16:42:21 +0900

    Fix： テストファイルをみやすく

--- OCR/test_codes/test1-1.py
+++ OCR/test_codes/test1-1.py
@@ -1,24 +1,12 @@
 import easyocr
 from PIL import Image, ImageDraw
-import cv2
 
 # リーダーオブジェクトに日本語と英語を設定
-reader = easyocr.Reader(['ja','en'],gpu = True)
+reader = easyocr.Reader(['ja','en'],gpu = False)
 
 # 画像のテキスト領域を解析
 def analyze_picture(target_path: str):
     draw_chararea(target_path, reader.readtext(target_path))
-
-def analyze_picture_tial(target_path: str):
-    image = cv2.imread(target_path)
-    # グレースケール化
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # 二値化（閾値は自動判別）
-    ret, img_thresh = cv2.threshold(gray_image, 0, 255, cv2.THRESH_OTSU)
-    # 自動判別した閾値を表示
-    print(f'閾値：{ret}')
-    cv2.imwrite("thresh.png", img_thresh)
-    draw_chararea("thresh.png", reader.readtext("thresh.png"))
 
 # 入力画像内に文字列の領域を赤枠で囲う
 def draw_chararea(target_path, results):
@@ -29,10 +17,10 @@ def draw_chararea(target_path, results):
         print(result)
         p0, p1, p2, p3 = result[0]
         draw.line([ *p0, *p1, *p2, *p3, *p0], fill='red', width=3)
-    image.save("draw_chararea.png")
+    image.save("draw_chararea_1.png")
 
 
 if __name__ == "__main__":
-    target_path = "test2.jpg"
-    print("test")
+    target_path = r".\samples\DSC_1936.JPG"
+    print("test1-1")
     analyze_picture(target_path)
---
+++ OCR/test_codes/test1-2.py
@@ -0,0 +1,35 @@
+import easyocr
+from PIL import Image, ImageDraw
+import cv2
+
+# リーダーオブジェクトに日本語と英語を設定
+reader = easyocr.Reader(['ja','en'],gpu = True)
+
+# 画像のテキスト領域を解析
+def analyze_picture_tial(target_path: str):
+    image = cv2.imread(target_path)
+    # グレースケール化
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # 二値化（閾値は自動判別）
+    ret, img_thresh = cv2.threshold(gray_image, 0, 255, cv2.THRESH_OTSU)
+    # 自動判別した閾値を表示
+    print(f'閾値：{ret}')
+    cv2.imwrite("thresh.png", img_thresh)
+    draw_chararea("thresh.png", reader.readtext("thresh.png"))
+
+# 入力画像内に文字列の領域を赤枠で囲う
+def draw_chararea(target_path, results):
+    image = Image.open(target_path).convert('RGB')
+    draw = ImageDraw.Draw(image)
+    # 座標情報からテキスト領域を四角で囲う
+    for result in results:
+        print(result)
+        p0, p1, p2, p3 = result[0]
+        draw.line([ *p0, *p1, *p2, *p3, *p0], fill='red', width=3)
+    image.save("draw_chararea_2.png")
+
+
+if __name__ == "__main__":
+    target_path = r".\samples\DSC_1936.JPG"
+    print("test1-2")
+    analyze_picture_tial(target_path)
